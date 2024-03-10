acf6b5808c563421f4ff9f049998ca7b15a5acb8
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-10 14:03:07 +0800

    initial test opens front page

--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -1,5 +1,6 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Note app', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5173')
+    cy.contains('Notes')
   })
 })
