b3580e1d4f205adb5739e7a8ebb0c34df53b10b3
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-10 14:27:59 +0800

    extract step visiting page to beforeEach block

--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -1,11 +1,12 @@
 describe('Note app', function() {
-  it('front page can be opened', function() {
+  beforeEach(function() {
     cy.visit('http://localhost:5173')
+  })
+  it('front page can be opened', function() {
     cy.contains('Notes')
   })
 
   it('login from can be opened', function () {
-    cy.visit('http://localhost:5173')
     cy.contains('Open Login').click()
   })
 })
