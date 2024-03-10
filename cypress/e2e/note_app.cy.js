b322f6b4efe72836380aaa25f5471e3e539ff1fc
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-10 19:00:40 +0800

    add test for username and password input and login

--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -9,4 +9,14 @@ describe('Note app', function() {
   it('login from can be opened', function () {
     cy.contains('Open Login').click()
   })
+
+  it('user can login', function() {
+    cy.contains('Open Login').click()
+    // cy.get('input:first').type('testuser')
+    // cy.get('input:last').type('hellluva')
+    cy.get('#username').type('testuser')
+    cy.get('#password').type('hellluva')
+    cy.get('#login-button').click()
+    cy.contains('Jend Schmidt logged in')
+  })
 })
